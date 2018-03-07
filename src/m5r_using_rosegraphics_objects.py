7ab2a49ffb7af6257a9f9a24de215bce669a6042
GodMasterLee <lib1@rose-hulman.edu>
2018-03-07 13:34:38 -0500

    Albert Lee Bowen Li

--- src/m5r_using_rosegraphics_objects.py
+++ src/m5r_using_rosegraphics_objects.py
@@ -5,8 +5,8 @@ This module uses ROSEGRAPHICS to demonstrate:
   -- accessing their DATA via INSTANCE VARIABLES.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Bowen Li.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ########################################################################
 #
@@ -17,34 +17,34 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #     a. For the RoseGraphics coordinate system:
 #
 #        -- Where is the (0, 0) point on the screen?
-#              WRITE_YOUR_ANSWER_HERE,_REPLACING_THIS
+#              I think it is in the left up corner
 #
 #        -- In what direction on the screen does the positive X-axis point?
-#              WRITE_YOUR_ANSWER_HERE,_REPLACING_THIS
+#             Form the left to the right
 #
 #        -- In what direction on the screen does the positive Y-axis point?
-#              WRITE_YOUR_ANSWER_HERE,_REPLACING_THIS
+#              From the top to the bottom
 #
 #     b. Write a line of code that constructs a basic RoseWindow object:
-#            WRITE_YOUR_ANSWER_HERE,_REPLACING_THIS
+#             rg.RoseWindow()
 #
 #     c. What is the default height of a RoseWindow?
 #          Type into  main  the code shown in your answer above.  That will
 #          ask PyCharm will help you figure out the answer to this question.
 #          Hint: After you type the   (   in the line of code,
 #          if you wait a moment PyCharm will add the   )   and has a popup.
-#            WRITE_YOUR_ANSWER_HERE,_REPLACING_THIS
+#            width = 400 & height = 300
 #
 #     d. Write a line of code that constructs a RoseWindow object whose
 #        height is 100 with any width you choose.
 #           Again try to use PyCharm's hints to help you figure this out.
-#               WRITE_YOUR_ANSWER_HERE,_REPLACING_THIS
+#               rg.RoseWindow(666,100)
 #
 #     e. Use the DOT trick to answer the following:
 #
 #          -- Write the names of two types of graphics objects
 #             that you can construct OTHER than Circle and Point:
-#                WRITE_YOUR_ANSWER_HERE,_REPLACING_THIS
+#                square & rectangle
 #
 #          -- Write the names of three METHODs that Circle objects have:
 #               Hint: Use the circle from the  example3  function below with
@@ -53,7 +53,7 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #          -- Write the names of three INSTANCE VARIABLEs
 #             that Circle objects have:
-#                WRITE_YOUR_ANSWER_HERE,_REPLACING_THIS
+#                center coordinate X, center coordinate Y, & radius
 #
 #     f. What does a RoseWindow RENDER method do?
 #            WRITE_YOUR_ANSWER_HERE,_REPLACING_THIS
@@ -105,6 +105,7 @@ def example2():
     point1 = rg.Point(100, 150)
     point2 = rg.Point(200, 50)
 
+
     # -------------------------------------------------------------------------
     # A RoseGraphics object is not associated with a window,
     # and hence are not drawn, until you ATTACH it to a window.
@@ -140,6 +141,7 @@ def example3():
     circle = rg.Circle(center_point, radius)
     circle.fill_color = 'green'
     circle.attach_to(window)
+
 
     # -------------------------------------------------------------------------
     # Rectangle: needs two opposite corners.
--- src/m6_using_objects.py
+++ src/m6_using_objects.py
@@ -5,8 +5,8 @@ This module lets you practice  ** using objects **, including:
   -- accessing their DATA via INSTANCE VARIABLES
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Bowen Li.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -14,6 +14,9 @@ import rosegraphics as rg
 def main():
     """ Calls the other functions to demonstrate and/or test them. """
     # Test your functions by putting calls to them here:
+    two_circles()
+    circle_and_rectangle()
+    lines()
 
 
 def two_circles():
@@ -26,8 +29,18 @@ def two_circles():
            -- One is filled with some color and one is not filled.
     -- Waits for the user to press the mouse, then closes the window.
     """
+    window = rg.RoseWindow(500,500,'This is fun')
+    center1 = rg.Point(150,60)
+    circle1 = rg.Circle(center1,50)
+    center2 = rg.Point(200,300)
+    circle2 = rg.Circle(center2,30)
+    circle2.fill_color = 'green'
+    circle1.attach_to(window)
+    circle2.attach_to(window)
+    window.render()
+    window.close_on_mouse_click()
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement this function, per its doc-string above.
+    # Done: 2. Implement this function, per its doc-string above.
     #    -- ANY two rg.Circle objects that meet the criteria are fine.
     #    -- File  COLORS.txt  lists all legal color-names.
     # Put a statement in   main   to test this function
@@ -66,8 +79,30 @@ def circle_and_rectangle():
            75.0
            150.0
     """
+    window = rg.RoseWindow(500,500,'This is a little tiring')
+    cp = rg.Point(150,60)
+    cirle = rg.Circle(cp,30)
+    cirle.fill_color = 'blue'
+    c1 = rg.Point(150,200)
+    c2 = rg.Point(250,300)
+    rec = rg.Rectangle(c1,c2)
+    cirle.attach_to(window)
+    rec.attach_to(window)
+    print(cirle.outline_thickness)
+    print(cirle.fill_color)
+    print(cirle.center)
+    print(cirle.center.x)
+    print(cirle.center.y)
+    print(rec.outline_thickness)
+    print(rec.fill_color)
+    print(rec.get_center())
+    print(rec.get_center().x)
+    print(rec.get_center().y)
+    window.render()
+    window.close_on_mouse_click()
+
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement this function, per its doc-string above.
+    # Done: 3. Implement this function, per its doc-string above.
     #   -- ANY objects that meet the criteria are fine.
     # Put a statement in   main   to test this function
     #    (by calling this function).
@@ -99,8 +134,24 @@ def lines():
 
     -- Waits for the user to press the mouse, then closes the window.
     """
+    window = rg.RoseWindow(500,500,'I am getting to this')
+    P1 = rg.Point(150,150)
+    P2 = rg.Point(250,250)
+    P3 = rg.Point(350,350)
+    P4 = rg.Point(450,450)
+    l1 = rg.Line(P1,P2)
+    l1.thickness = 5
+    l2 = rg.Line(P3,P4)
+    print(l1.get_midpoint())
+    print(l1.get_midpoint().x)
+    print(l1.get_midpoint().y)
+    l1.attach_to(window)
+    l2.attach_to(window)
+    window.render()
+    window.close_on_mouse_click()
+
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # Done: 4. Implement and test this function.
     # -------------------------------------------------------------------------
 
 
